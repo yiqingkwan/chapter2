fa0c6b802db4d4b555db381c988270c2c16dd855
Natalie Kwan <natalie@swuser-129-8-192-169.swlan.csufresno.edu>
2022-02-22 14:17:03 -0800

    initial commit

---
+++ 2_1.py
@@ -0,0 +1,22 @@
+# print(3 + 2, 3 - 2, 3 * 2)
+# print(8 / 2, 8 ** 2, 2 * (3 + 4))
+
+
+# speed = 50
+# time_elasped = 14
+# distance = speed * time_elasped
+# print(distance)
+
+
+# a = 2
+# b = 3
+# print(abs(1 - (4*b)))
+# print(int((a ** b) + .8))
+# print(round(a / b, 3))
+
+
+total_inches = 41
+feet = total_inches // 12
+inches = total_inches % 12
+print(feet, inches)
+
